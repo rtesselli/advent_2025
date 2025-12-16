bb8c0eed58f8341056696a4b18a27b9428c16e9a
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2025-12-16 17:11:33 +0100

    Add day 03 part 2

--- day03.py
+++ day03.py
@@ -17,10 +17,27 @@ def max_joltage(bank: str) -> int:
     return int(f"{max_c}{second_max_c}")
 
 
+def max_joltage_rec(bank: str, left_chars: int) -> str:
+    if left_chars == 1:
+        return max_char(bank)
+    max_c = max_char(bank[:-left_chars + 1])
+    idx_max_c = bank.index(max_c)
+    return max_c + max_joltage_rec(bank[idx_max_c + 1:], left_chars - 1)
+
+
+def max_joltage2(bank: str) -> int:
+    return int(max_joltage_rec(bank, 12))
+
+
 def part1(banks: list[str]) -> int:
     return sum(max_joltage(bank) for bank in banks)
+
+
+def part2(banks: list[str]) -> int:
+    return sum(max_joltage2(bank) for bank in banks)
 
 
 if __name__ == '__main__':
     data = parse_input()
     print(part1(data))
+    print(part2(data))
