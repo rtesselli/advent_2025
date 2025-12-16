7294fb9f150e8840437641f766d5c3e7accea07c
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2025-12-16 17:12:49 +0100

    Simplify day03

--- day03.py
+++ day03.py
@@ -7,16 +7,6 @@ def max_char(s: str) -> str:
     return str(max(int(digit) for digit in s))
 
 
-def max_joltage(bank: str) -> int:
-    max_c = max_char(bank)
-    idx_max_c = bank.index(max_c)
-    while idx_max_c == len(bank) - 1:
-        max_c = max_char(f"{bank[:idx_max_c]}{bank[idx_max_c + 1:]}")
-        idx_max_c = bank.index(max_c)
-    second_max_c = max_char(bank[idx_max_c + 1:])
-    return int(f"{max_c}{second_max_c}")
-
-
 def max_joltage_rec(bank: str, left_chars: int) -> str:
     if left_chars == 1:
         return max_char(bank)
@@ -25,16 +15,16 @@ def max_joltage_rec(bank: str, left_chars: int) -> str:
     return max_c + max_joltage_rec(bank[idx_max_c + 1:], left_chars - 1)
 
 
-def max_joltage2(bank: str) -> int:
-    return int(max_joltage_rec(bank, 12))
+def max_joltage(bank: str, left_chars: int) -> int:
+    return int(max_joltage_rec(bank, left_chars))
 
 
 def part1(banks: list[str]) -> int:
-    return sum(max_joltage(bank) for bank in banks)
+    return sum(max_joltage(bank, 2) for bank in banks)
 
 
 def part2(banks: list[str]) -> int:
-    return sum(max_joltage2(bank) for bank in banks)
+    return sum(max_joltage(bank, 12) for bank in banks)
 
 
 if __name__ == '__main__':
